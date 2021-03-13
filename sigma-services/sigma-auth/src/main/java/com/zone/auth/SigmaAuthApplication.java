8768ab2e186a049359e86d652895932ad70f6d0a
Jone <jyong_zhu@163.com>
2021-03-13 20:38:26 +0800

    update 采用logback作为slf4j的实现

--- sigma-services/sigma-auth/src/main/java/com/zone/auth/SigmaAuthApplication.java
+++ sigma-services/sigma-auth/src/main/java/com/zone/auth/SigmaAuthApplication.java
@@ -1,5 +1,6 @@
 package com.zone.auth;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Date: 2021/1/17 6:56 下午
  * @Description:
  */
+@MapperScan(basePackages = "com.zone.*.infrastructure.db.mapper,")
 @SpringBootApplication
 public class SigmaAuthApplication {
 
